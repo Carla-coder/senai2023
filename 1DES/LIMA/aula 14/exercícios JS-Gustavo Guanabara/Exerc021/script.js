55ec001f4d42c5fb03df64298b862e8416be6772
Carla-coder <cmartezanato@yahoo.com.br>
2023-11-28 10:50:48 -0300

    Att

---
+++ 1DES/LIMA/aula 14/exercícios JS-Gustavo Guanabara/Exerc021/script.js
@@ -0,0 +1,10 @@
+function contar() {
+    let contagem = "";
+    for (let i = 1; i <= 10; i++) {
+        contagem += i + " ";
+    }
+    document.getElementById("saida").innerHTML = "<p>" + contagem + "</p>";
+    saida.innerHTML += ` &#x1F3C1;`
+}
+
+
