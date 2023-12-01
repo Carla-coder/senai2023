63e9fdfa8a535c97120f03aa0d0d84492c636553
Carla-coder <cmartezanato@yahoo.com.br>
2023-11-30 21:43:30 -0300

    Att

---
+++ 1DES/LIMA/Portifólio/reveal.js
@@ -0,0 +1,72 @@
+window.revelar = ScrollReveal({reset:true})
+
+// TOPO DO SITE
+revelar.reveal('.efeito-txt-topo',
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+// OS TÍTULOS DAS SESSÕES
+
+revelar.reveal('.título',
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+//  SESSÃO DE ESPECIALIDADES
+
+revelar.reveal('.efeito-especialidades1',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-especialidades2',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-especialidades3',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+// SESSÃO SOBRE
+
+revelar.reveal('.efeito-img-sobre',
+{
+    duration: 1000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-txt-sobre',
+{
+    duration: 1000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'hight'
+})
+
+
+
+
+
+
+
+
